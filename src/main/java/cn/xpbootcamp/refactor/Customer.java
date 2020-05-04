4f51415f3d45848f284e0fb91d6b8d221a87a580
Hong Liu <hong.liu1@Hongs-MacBook-Pro.local>
2020-05-04 11:46:44 +0800

    extract get frequentRenterPoints function to Rental class

--- src/main/java/cn/xpbootcamp/refactor/Customer.java
+++ src/main/java/cn/xpbootcamp/refactor/Customer.java
@@ -32,9 +32,7 @@ public class Customer {
             //show figures for this rental
            
             //add frequent renter points
-            frequentRenterPoints++;
-            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1)
-                frequentRenterPoints++;
+            frequentRenterPoints += each.getFrequentRenterPoints();
 
             //show figures for this rental
             result.append("\t")
